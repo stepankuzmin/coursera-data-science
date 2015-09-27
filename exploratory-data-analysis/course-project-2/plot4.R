7e2949721b0226e24a421eeb4710bcd9f91946af
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2015-09-27 18:57:19 +0500

    plot4

---
+++ exploratory-data-analysis/course-project-2/plot4.R
@@ -0,0 +1,25 @@
+# Across the United States, how have emissions from coal
+# combustion-related sources changed from 1999–2008?
+library(dplyr)
+library(ggplot2)
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+df <- tbl_df(NEI) %>%
+      merge(SCC, by = "SCC") %>%                                  # merge source classification codes
+      filter(grepl("Comb.*Coal", EI.Sector, ignore.case = T)) %>% # filter coal combustion-related sources
+      group_by(year) %>%                                          # group observations by year
+      summarise_each(funs(sum), Emissions) %>%                    # calculate sum of emissions by year
+      mutate_each(funs(. / 1000), Emissions)                      # convert tons of emissions to thousands of tons
+
+png(file = "plot4.png")
+
+g <- ggplot(df, aes(x=factor(year), Emissions))
+g + geom_bar(aes(fill=year), stat = "identity") +
+    guides(fill=FALSE) +
+    labs(x = "Year") +
+    labs(y = expression("Total PM"[2.5] * " Emission (in thousands tons)")) +
+    labs(title = expression("Coal combustion-related PM"[2.5] * " Emission (1999 — 2008)"))
+
+dev.off()
